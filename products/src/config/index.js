113c0f17ea7127ad2d31070faa9ae1f84b432253
kpbarasa <65596472+kpbarasa@users.noreply.github.com>
2022-11-28 05:53:20 +0300

    Update Yml

--- products/src/config/index.js
+++ products/src/config/index.js
@@ -1,13 +1,17 @@
 const dotEnv  = require("dotenv");
 
-// if (process.env.NODE_ENV !== 'prod') {
-//     const configFile =  `./.env.${process.env.NODE_ENV}`;
-//     dotEnv.config({ path:  configFile });
-// } else {
-//     dotEnv.config({ path: '.env' });
-// }
 
-dotEnv.config({ path: '.env' });
+if (process.env.NODE_ENV !== 'prod') {
+
+    const configFile =  `.env`;
+    dotEnv.config({ path: '.env' });
+
+
+} else {
+    
+    dotEnv.config();
+
+}
 
 module.exports = {
     PORT: process.env.PORT,
@@ -15,7 +19,7 @@ module.exports = {
     APP_SECRET: process.env.APP_SECRET,
     MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
     CLOUDAMQP_URL: process.env.CLOUDAMQP_URL,
-    EXCHANGE_NAME: 'ONLINE_SHOPPING',
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
     SHOPPING_BINDING_KEY: 'SHOPPING_SERVICE',
     CUSTOMER_BINDIG_KEY: 'CUSTOMER_SERVICE'
 }
